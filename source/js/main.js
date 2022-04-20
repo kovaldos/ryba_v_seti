b02146f8d0f4abe20b08590d6ff4ea86eb2a35e2
Yaroslav Kovalev <yakovaldos@yandex.ru>
2022-04-20 11:18:16 +0300

    removed unnecessary files

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {initCustomSelect} from './modules/form/init-custom-select';
-import {initFormValidate} from './modules/form/init-form-validate';
+// import {iosVhFix} from './utils/ios-vh-fix';
+// import {initModals} from './modules/modals/init-modals';
+// import {initCustomSelect} from './modules/form/init-custom-select';
+// import {initFormValidate} from './modules/form/init-form-validate';
 
 // ---------------------------------
 
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Utils
   // ---------------------------------
 
-  iosVhFix();
+  // iosVhFix();
 
   // Modules
   // ---------------------------------
@@ -18,9 +18,9 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
-    initCustomSelect();
-    initFormValidate();
+    // initModals();
+    // initCustomSelect();
+    // initFormValidate();
   });
 });
 
